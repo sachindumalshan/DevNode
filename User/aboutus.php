99c611f28393a9fa71b3c29d12dff3b0a68c3aa1
Sachindu Malshan <sachindu.info@gmail.com>
2023-09-12 10:26:17 +0530

    About us page was created

---
+++ User/aboutus.php
@@ -0,0 +1,158 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- Font awesome icons -->
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+
+    <!-- CSS file link -->
+    <link rel="stylesheet" type="text/css" href="CSS/style.css">
+
+    <title>DevNode</title>
+  </head>
+  <body>
+
+    <!-- Navigation Bar -->
+    <nav class="navbar navbar-expand-sm navbar-dark fixed-top" style="background-color:#082b8f">
+        <div class="container">
+            <a href="index.html" class="navbar-brand">
+                <img src="img/devnode.png" class="me-2" height="40" alt="DevNode" loading="lazy"/>
+            </a>
+            <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a href="index.html" class="nav-link active" >Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="aboutus.html" class="nav-link">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="contactus.html" class="nav-link">Contact Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="profile.html" class="nav-link"><i class="fas fas fa-user-circle"></i> Profile</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <br><br><br>
+    <!-- About Us -->
+    <section id = "aboutus-box">
+      <div class="container">
+        <div class="row">
+          <div class="col-2"></div>
+          <div class="col-8">
+            <div class="mt-4">
+              <h2 class="text-center">Who We Are?</h2>
+              <div class="text-center my-4">
+                <img src="img/devnode.png" class="img-fluid" style="background-color:#082b8f; border-radius: 20px">
+              </div>
+              <div>
+                <h4>Welcome to DevNode - Your Tech Journey Begins Here</h4>
+                <p>
+                  At DevNode, we're passionate about technology and committed to bringing you the latest insights, guides, and news from the ever-evolving world of tech.Our mission is to be your trusted source for all things tech-related. Whether you're a seasoned developer, a curious gadget enthusiast, or simply someone looking to stay updated on the tech trends that shape our world, you've come to the right place.
+                </p>
+                <h4>What Sets Us Apart?</h4>
+                <p>
+                  <strong>Expertise:</strong> Our team of tech enthusiasts and experts is dedicated to researching, testing, and delivering accurate and insightful content. We're here to simplify complex topics, provide in-depth reviews, and offer practical solutions.
+                </p>
+                <p>
+                  <strong>Diverse Content:</strong> DevNode covers a wide spectrum of tech topics, from coding and programming languages to the latest hardware and software releases. We cater to beginners and professionals alike, ensuring there's something for everyone.
+                </p>
+                <p>
+                  <strong>Community:</strong> We believe in fostering a vibrant tech community. Join us in discussions, ask questions, and share your knowledge with like-minded individuals. DevNode is not just a website; it's a community of tech enthusiasts supporting each other's growth.
+                </p>
+
+                <h4>Our Vision</h4>
+                <p>
+                  Our vision is to empower you with knowledge, inspire your tech journey, and keep you informed about the innovations that shape our digital landscape. Technology is more than just tools and gadgets; it's a transformative force that touches every aspect of our lives, and we're here to explore its endless possibilities with you.
+                </p>
+
+                <h4>Get in Touch</h4>
+                <p>We'd love to hear from you. Whether you have questions, feedback, or ideas for future content, don't hesitate to reach out to us. Together, let's navigate the exciting world of technology.</p>
+
+                <p>Thank you for being part of the DevNode community. Let's embark on this tech adventure together!</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-2"></div>
+        </div>
+      </div>
+    </section>
+
+    <div>
+      <!-- Middle footer -->
+      <section id="detail-section" class="pt-4 text-white" style="background-color: #082b8f;">
+          <div class="container">
+              <div class="row">
+                  <div class="col-md-3 footer-link">
+                      <h5>Quick Links</h5>
+                        <a class="detail_link" href="aboutus.html" ><h6 class="footer-link-text">About Us</h6></a>
+                        <a class="detail_link" href="contactus.html" ><h6 class="footer-link-text">Contact Us</h6></a>
+                        <a class="detail_link" href="faq.html" ><h6 class="footer-link-text">FAQ</h6><br></a>
+                        <a class="detail_link" href="term.html" ><h6 class="footer-link-text">Terms and Conditions</h6></a>
+                  </div>
+
+                  <hr class="ml-3 d-block d-sm-none" style="width: 75%; background-color: white;" >
+
+                  <div class="col-md-3 ">
+                      <div>
+                          <i class="fas fa-map-marker-alt"></i> Location :
+                          <h6 class="mt-2">DevNode (PVT) LTD,<br> No.201, Pahala Bomiriya,<br>Kaduwela.<br>10640</h6>
+                      </div>
+                      <div class="mt-4">
+                          <i class="fas fa-phone-square-alt"></i> Telephone :
+                          <h6 class="mt-2">Work &ensp;&nbsp;: +94 71 760 9016<br>Mobile : +94 76 616 6355</h6>
+                      </div>
+                  </div>
+                  <hr class="ml-3 d-block d-sm-none" style="width: 75%; background-color: white;" >
+
+                  <div class="col-md-6 ">
+                       <h3>NEWSLETTER</h3>
+                       <p class="my-4 text-justify">Stay in the loop with our newsletter! Get the latest updates, articles, and insights from our blog delivered right to your inbox. Join our community of readers and never miss out on valuable content again. Subscribe now and be part of our blog's journey!</p>
+                      <div class="input-group mb-4 col-md-9 pl-0">
+                          <input type="text" class="form-control" placeholder="Your Email Address"/>
+                          <button class="btn btn-primary" type="button">Subscribe</button>
+                      </div>
+                  </div>
+                  <hr class="ml-3 d-block d-sm-none" style="width: 75%; background-color: white;">
+                  <hr class="d-none d-md-block mt-2" style="color: whitesmoke; height: 3px;">
+              </div>
+          </div>
+      </section>
+
+      <!-- Footer -->
+      <footer id="main-footer" class="py-1 text-white" style="background-color: #082b8f;">
+        <div class="container">
+          <div class="row">
+            <div class="col">
+            <p>Copyright &copy;
+                <span id="year"></span> DevNode | All Rights Reserved</p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
+    <script>
+    // Get the current year for the copyright
+    $('#year').text(new Date().getFullYear());
+    </script>
+
+  </body>
+</html>
