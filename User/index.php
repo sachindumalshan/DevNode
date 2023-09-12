969838673f46d1b7110cfd9177c20e62478d82fc
Sachindu Malshan <sachindu.info@gmail.com>
2023-09-12 09:59:56 +0530

    style.css file location was changed

--- User/index.php
+++ User/index.php
@@ -12,12 +12,12 @@
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
 
     <!-- CSS file link -->
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" type="text/css" href="CSS/style.css">
 
     <title>DevNode</title>
   </head>
   <body>
-    
+
     <!-- Navigation Bar -->
     <nav class="navbar navbar-expand-sm navbar-dark fixed-top" style="background-color:#082b8f">
         <div class="container">
@@ -52,12 +52,12 @@
       <!-- Middle footer -->
       <section id="detail-section" class="pt-4 text-white" style="background-color: #082b8f;">
           <div class="container">
-              <div class="row"> 
+              <div class="row">
                   <div class="col-md-3 footer-link">
                       <h5>Quick Links</h5>
                         <a class="detail_link" href="aboutus.html" ><h6 class="footer-link-text">About Us</h6></a>
                         <a class="detail_link" href="contactus.html" ><h6 class="footer-link-text">Contact Us</h6></a>
-                        <a class="detail_link" href="faq.html" ><h6 class="footer-link-text">FAQ</h6><br></a>                  
+                        <a class="detail_link" href="faq.html" ><h6 class="footer-link-text">FAQ</h6><br></a>
                         <a class="detail_link" href="term.html" ><h6 class="footer-link-text">Terms and Conditions</h6></a>
                   </div>
 
@@ -101,8 +101,8 @@
         </div>
       </footer>
     </div>
-    
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>    
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 
